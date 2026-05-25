b3321f196871b71c8910e5b2ed9514674a09b676
Palak Majethia <palakmajethia19@gmail.com>
2026-05-25 16:09:15 +0530

    Implement Grayscale image processing

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import cv2
+
 img = cv2.imread("images/test.jpg")
-cv2.imshow("METER", img)
+
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Original", img)
+cv2.imshow("Gray", gray)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
