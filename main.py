ab4435a4839989e6fee79029ecccb6b0b5a397eb
Palak Majethia <palakmajethia19@gmail.com>
2026-05-25 16:18:35 +0530

    Add edge detection using Canny

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import cv2
 
 img = cv2.imread("images/test.jpg")
 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+blur = cv2.GaussianBlur(gray, (5,5),0)
+edges = cv2.Canny(blur, 50, 150)
 
-cv2.imshow("Original", img)
-cv2.imshow("Gray", gray)
+cv2.imshow("Edges",edges)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
