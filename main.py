fe73b39193e112d256d62780018b5432445f0d6c
Palak Majethia <palakmajethia19@gmail.com>
2026-05-25 16:28:35 +0530

    Implement analog dial circle detection

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import cv2
+import numpy as np
 
 img = cv2.imread("images/test.jpg")
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 blur = cv2.GaussianBlur(gray, (5,5),0)
-edges = cv2.Canny(blur, 50, 150)
 
-cv2.imshow("Edges",edges)
+circles = cv2.HoughCircles(
+    gray,
+    cv2.HOUGH_GRADIENT,
+    1
+    100
+    param1=50
+    param2=30
+    minRadius=50
+    maxRadius=300
+)
+
+if circles is not None:
+    circles= np.uint16(np.around(circles))
+    for i in circles[0, :]:
+
+        cv2.circle(img, (i[0],i[1]), i[2], (0,255,0) , 2)
+
+         cv2.circle(img, (i[0],i[1]), 2, (0,255,0) , 3)
+        
+cv2.imshow("Circle Detection", img)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
