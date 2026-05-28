a4d2a7f11f10d20081618a4f1ea94af9a3b1be76
Palak Majethia <palakmajethia@gmail.com>
2026-05-28 13:47:48 +0530

    Separate preprocessing pipeline into reusable module

---
+++ src/preprocessing.py
@@ -0,0 +1,7 @@
+import cv2
+def preprocessing_image(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    edges = cv2.Canny(blur, 50,150)
+    return gray, blur, edges
+    
